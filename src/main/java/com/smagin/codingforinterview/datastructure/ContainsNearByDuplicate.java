8d39c02e47c5dbd3443fa68f49eab5dbdd8da674
evgenij <zhenyasmagin@yandex.ru>
2019-07-07 12:08:08 +0300

    Contains Near By duplicate

---
+++ src/main/java/com/smagin/codingforinterview/datastructure/ContainsNearByDuplicate.java
@@ -0,0 +1,23 @@
+package com.smagin.codingforinterview.datastructure;
+
+
+//Given an array of integers and an integer k, find out whether there are two distinct indices i and
+//j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+
+public class ContainsNearByDuplicate {
+    public int[] containsNearByDuplicate(int[] nums, int k){
+        int[] result = new int[2];
+        int length = result.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                if((nums[i] == nums[j]) && (Math.abs(i-j) > k)){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
+    }
+}
