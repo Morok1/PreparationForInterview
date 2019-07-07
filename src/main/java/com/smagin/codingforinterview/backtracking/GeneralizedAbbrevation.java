da4cf7265836c697cca2cc81f8a5f54663b68807
evgenij <zhenyasmagin@yandex.ru>
2019-07-07 14:32:42 +0300

    add generalizedAbbrevation

---
+++ src/main/java/com/smagin/codingforinterview/backtracking/GeneralizedAbbrevation.java
@@ -0,0 +1,27 @@
+package com.smagin.codingforinterview.backtracking;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GeneralizedAbbrevation {
+    public List<String> generilize(String word){
+        List<String> strings = new ArrayList<>();
+        Character[] characters = new Character[]{'a', 'b', 'c'};
+
+        String newWord = generate(word, characters[new Random().nextInt(characters.length)]);
+        strings.add(newWord)
+        return strings;
+    }
+
+    public String generate(String word, Character randomCharacter){
+        char[] characters = word.toCharArray();
+        int randomCharacterInCharacters = new Random().nextInt(characters.length);
+
+        characters[randomCharacterInCharacters]  = randomCharacter;
+        String newWord = new StringBuilder().append(characters).toString();
+
+        return newWord;
+    }
+}
