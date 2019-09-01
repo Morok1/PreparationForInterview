1f869c9a5908535f86e0d907df8bee5f1d5b1821
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-09-01 17:49:41 +0300

    correct solution

--- src/main/java/codinginterview/Q5.java
+++ src/main/java/codinginterview/Q5.java
@@ -7,17 +7,26 @@ import java.util.Map;
 the array. How do you find the duplicated number? For example, if an array with length 5 contains numbers {0, 2,
 1, 3, 2}, the duplicated number is 2. */
 public class Q5 {
-    public int findDuplicate(int[] array){
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < array.length ; i++) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+    public static int findDuplicate(int[] array){
+        if(array == null || array.length == 0){
+            throw new IllegalStateException();
         }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length ; i++) { map.put(array[i], map.getOrDefault(i, 0) + 1); }
         for (int i = 0; i < array.length; i++) {
-            if(map.get(i) > 1){
-                return map.get(i);
+            if(map.getOrDefault(array[i], 0) > 1){
+                return map.get(array[i]);
             }
         }
         throw new IllegalStateException("No have this element!");
 
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2, 3, 3};
+
+        int duplicateElement = findDuplicate(array);
+        System.out.println(duplicateElement);
     }
 }
