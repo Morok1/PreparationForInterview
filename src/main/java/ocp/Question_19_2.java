d4ee03522bed2c268766cd89510d981689a790af
e <zhenyasmagin@yandex.ru>
2019-10-03 22:32:50 +0300

    add questions 19

---
+++ src/main/java/ocp/Question_19_2.java
@@ -0,0 +1,11 @@
+package ocp;
+
+public class Question_19_2 {
+    public static void main(String[] args) {
+        try {
+            // Do nothing
+        } finally {
+            // Do nothing
+        }
+    }
+}
