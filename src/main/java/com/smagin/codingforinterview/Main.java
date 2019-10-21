b0663d3398e6d85781e6ccd71ef3b1c2891af6d1
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-10-21 21:28:31 +0300

    add unused

--- src/main/java/com/smagin/codingforinterview/Main.java
+++ src/main/java/com/smagin/codingforinterview/Main.java
@@ -3,7 +3,6 @@ package com.smagin.codingforinterview;
 public class Main {
     public static void main(String[] args) {
         int a = (16 >> 1) | 1;
-        System.out.println("Hello world!" + a);
 
     }
 }
